5060063cdbc0ff77e3778f380b4a07e9bd4240b9
hydenz <marcelof_lima@hotmail.com>
2021-03-09 17:57:27 -0300

    Ajustes finais

--- api.js
+++ api.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'development') {
   );
 } else {
   app.use(express.static(path.join(__dirname, 'build')));
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
