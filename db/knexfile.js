b9a9ae0de695b7306958bb323962c1e3c5ee27b8
hydenz <marcelof_lima@hotmail.com>
2021-03-09 18:22:01 -0300

    Migração para o postgresql

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,12 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-  client: 'mysql',
-  connection: process.env.DATABASE_URL || {
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: "kool_shoes",
-  }
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: './migrations',
+  },
+  seeds: { directory: './seeds' },
 };
